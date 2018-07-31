044bf9c6a9a01f64ef033112f43c279e6e1d45cc
davecremins <davecremins@gmail.com>
2018-07-31 21:22:52 +0100

    Add tests for interface conversion

--- keygen_test.go
+++ keygen_test.go
@@ -3,6 +3,7 @@ package keyMgt
 import (
 	"testing"
 	//"fmt"
+	"reflect"
 )
 
 func TestBadBitSize(t *testing.T) {
@@ -15,5 +16,20 @@ func TestBadBitSize(t *testing.T) {
 	}
 }
 
-func TestConversionOfPublicKey(t *testing.T){}
-func TestConversionOfPrivateKey(t *testing.T){}
+func TestConversionOfPrivateKey(t *testing.T){
+	privateKey, _, _ := CreateRSAKeys(1024)
+	iType := ConvertPrivateKeyToInterface(privateKey)
+	typeIs := reflect.TypeOf(iType).String()
+	if typeIs != "*rsa.PrivateKey" {
+		t.Errorf("type check for *rsa.PrivateKey failed, got %s instead", typeIs)
+	}
+}
+
+func TestConversionOfPublicKey(t *testing.T){
+	_, publicKey, _ := CreateRSAKeys(1024)
+	iType := ConvertPublicKeyToInterface(publicKey)
+	typeIs := reflect.TypeOf(iType).String()
+	if typeIs != "*rsa.PublicKey" {
+		t.Errorf("type check for *rsa.PublicKey failed, got %s instead", typeIs)
+	}
+}
