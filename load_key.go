1478e32a08557154dee6ce7b33a5d24569f74fdb
davecremins <davecremins@gmail.com>
2018-08-02 22:07:17 +0100

    Use correct function to create pem block for public key

--- load_key.go
+++ load_key.go
@@ -1,20 +1,32 @@
 package keyMgt
 
 import (
+	"bufio"
 	"fmt"
-	"io/ioutil"
+	"os"
+	//"encoding/base64"
 	"crypto/rsa"
 	"crypto/x509"
 	"encoding/pem"
+	"errors"
+	"io/ioutil"
 )
 
-func LoadPublicFromFile(file string) (*rsa.PublicKey, error) {
-	data, err := ioutil.ReadFile(file)
-	if err != nil {
-		panic(err)
-	}
+func LoadPublicKeyFromPemFile(file string) (*rsa.PublicKey, error) {
+	fmt.Println("Loading key from", file)
 
-	block, _ := pem.Decode(data)
+	f, _ := os.Open(file)
+	defer f.Close()
+	fileInfo, _ := f.Stat()
+	size := fileInfo.Size()
+
+	pemBytes := make([]byte, size)
+	buffer := bufio.NewReader(f)
+	_, err := buffer.Read(pemBytes)
+
+	fmt.Println("Pem data as string", string(pemBytes))
+
+	block, _ := pem.Decode([]byte(pemBytes))
 	if block == nil {
 		return nil, errors.New("fatal: public key decoding error")
 	}
@@ -23,7 +35,24 @@ func LoadPublicFromFile(file string) (*rsa.PublicKey, error) {
 	if err != nil {
 		return nil, err
 	}
-	pub := pubInterface.(*rsa.PublicKey)
+	publicKey := pubInterface.(*rsa.PublicKey)
 
-	return &pub, nil
+	return publicKey, nil
+}
+
+func LoadPrivateKeyFromPemFile(file string) (*rsa.PrivateKey, error) {
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		panic(err)
+	}
+	block, _ := pem.Decode([]byte(data))
+	if block == nil {
+		return nil, errors.New("fatal: private key decoding error")
+	}
+	privateKey, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+
+	return privateKey, nil
 }
