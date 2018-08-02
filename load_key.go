960a4109cbc520b0918b109828b07df9a4de8d3a
davecremins <davecremins@gmail.com>
2018-08-02 23:13:18 +0100

    Refactor functions for load module

--- load_key.go
+++ load_key.go
@@ -4,55 +4,65 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	//"encoding/base64"
 	"crypto/rsa"
 	"crypto/x509"
 	"encoding/pem"
 	"errors"
-	"io/ioutil"
 )
 
-func LoadPublicKeyFromPemFile(file string) (*rsa.PublicKey, error) {
-	fmt.Println("Loading key from", file)
-
-	f, _ := os.Open(file)
-	defer f.Close()
-	fileInfo, _ := f.Stat()
-	size := fileInfo.Size()
-
-	pemBytes := make([]byte, size)
-	buffer := bufio.NewReader(f)
-	_, err := buffer.Read(pemBytes)
-
-	fmt.Println("Pem data as string", string(pemBytes))
-
-	block, _ := pem.Decode([]byte(pemBytes))
-	if block == nil {
-		return nil, errors.New("fatal: public key decoding error")
+func LoadPublicKeyFromPemFile(fileName string) (*rsa.PublicKey, error) {
+	pemBytes := loadKeyFromFile(fileName)
+	block, err := decodeBytesToPemBlock(&pemBytes)
+	if err != nil {
+		panic(err)
 	}
-
 	pubInterface, err := x509.ParsePKIXPublicKey(block.Bytes)
 	if err != nil {
 		return nil, err
 	}
 	publicKey := pubInterface.(*rsa.PublicKey)
-
 	return publicKey, nil
 }
 
-func LoadPrivateKeyFromPemFile(file string) (*rsa.PrivateKey, error) {
-	data, err := ioutil.ReadFile(file)
+func LoadPrivateKeyFromPemFile(fileName string) (*rsa.PrivateKey, error) {
+	pemBytes := loadKeyFromFile(fileName)
+	block, err := decodeBytesToPemBlock(&pemBytes)
 	if err != nil {
 		panic(err)
-	}
-	block, _ := pem.Decode([]byte(data))
-	if block == nil {
-		return nil, errors.New("fatal: private key decoding error")
 	}
 	privateKey, err := x509.ParsePKCS1PrivateKey(block.Bytes)
 	if err != nil {
 		return nil, err
 	}
-
 	return privateKey, nil
+}
+
+func loadKeyFromFile(fileName string) []byte {
+	fmt.Println("Will load key from", fileName)
+	pemFile, err := os.Open(fileName)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	defer pemFile.Close()
+
+	pfInfo, _ := pemFile.Stat()
+	size := pfInfo.Size()
+	pemBytes := make([]byte, size)
+
+	buffer := bufio.NewReader(pemFile)
+	buffer.Read(pemBytes)
+
+	fmt.Println("Pem data read successfully.")
+	fmt.Println(string(pemBytes))
+	return pemBytes
+}
+
+func decodeBytesToPemBlock(pemBytes *[]byte) (*pem.Block, error) {
+	block, _ := pem.Decode(*pemBytes)
+	if block == nil {
+		return nil, errors.New("fatal: key decoding error")
+	}
+	return block, nil
 }
