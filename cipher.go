e5f69414b8acd4a0bd8858e861e8dcf24aee2e52
davecremins <davecremins@gmail.com>
2018-08-08 21:06:35 +0100

    Return ciphertext/plaintext for encrypt/decrypt funcs

--- cipher.go
+++ cipher.go
@@ -4,36 +4,30 @@ import (
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/sha256"
-	"fmt"
-	"os"
 )
 
-func RSAEncrypt(content *[]byte, publicKey *rsa.PublicKey){
+func RSAEncrypt(content *[]byte, publicKey *rsa.PublicKey) ([]byte, error) {
 	label := []byte("")
 	hash := sha256.New()
 
 	ciphertext, err := rsa.EncryptOAEP(hash, rand.Reader, publicKey, *content, label)
 
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		return nil, err
 	}
 
-	fmt.Printf("OAEP encrypted [%s] to \n[%x]\n", string(*content), ciphertext)
-	fmt.Println()
+	return ciphertext, nil
 }
 
-func RSADecrypt(ciphertext *[]byte, privateKey *rsa.PrivateKey){
+func RSADecrypt(ciphertext *[]byte, privateKey *rsa.PrivateKey) ([]byte, error) {
 	label := []byte("")
 	hash := sha256.New()
 
 	plainText, err := rsa.DecryptOAEP(hash, rand.Reader, privateKey, *ciphertext, label)
 
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		return nil, err
 	}
 
-	fmt.Printf("OAEP decrypted [%x] to \n[%s]\n", ciphertext, plainText)
-	fmt.Println()
+	return plainText, nil
 }
