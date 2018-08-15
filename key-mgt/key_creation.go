252a31c97281898534605909b1bc9ef48725d923
davecremins <davecremins@gmail.com>
2018-08-15 21:38:16 +0100

    Add AES random key gen func & test

--- key-mgt/key_creation.go
+++ key-mgt/key_creation.go
@@ -1,9 +1,16 @@
 package keymgt
 
 import (
+	"errors"
 	"crypto/rand"
 	"crypto/rsa"
 )
+
+var supportedAESKeySizes = map[int]bool{
+	128: true,
+	192: true,
+	256: true,
+}
 
 func CreateRSAKeys(keySize int) (*rsa.PrivateKey, *rsa.PublicKey, error) {
 	privateKey, err := rsa.GenerateKey(rand.Reader, keySize)
@@ -23,4 +30,16 @@ func ConvertPrivateKeyToInterface(privateKey *rsa.PrivateKey) interface{} {
 	var iType interface{}
 	iType = privateKey
 	return iType
+}
+
+func CreateAESKey(keySize int) (string, error) {
+	if !supportedAESKeySizes[keySize] {
+		return "", errors.New("AES key size only supports 128, 192 or 256 bits")
+	}
+	key := make([]byte, keySize/8)
+	_, err := rand.Read(key)
+	if err != nil {
+		return "", err
+	}
+	return string(key), nil
 }
