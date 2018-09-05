9a8e6a6b23b31c849e1ee4efce7cb63f7b94042f
Dave Cremins <davecremins@gmail.com>
2018-09-05 23:16:21 +0100

    Include test for EOF panic

--- files/file_chunk_test.go
+++ files/file_chunk_test.go
@@ -1,10 +1,10 @@
 package files
 
 import (
-	"strings"
 	"bytes"
-	"testing"
+	"strings"
 	"sync"
+	"testing"
 )
 
 func TestChunkingOfReader(t *testing.T) {
@@ -16,7 +16,7 @@ func TestChunkingOfReader(t *testing.T) {
 	buffer := read(reader, part1, wg)
 
 	expected := []byte("Testing")
-	if !bytes.Equal(expected, buffer){
+	if !bytes.Equal(expected, buffer) {
 		t.Error("Read failed to extract correct data for part 1")
 	}
 
@@ -24,7 +24,25 @@ func TestChunkingOfReader(t *testing.T) {
 	buffer = read(reader, part2, wg)
 
 	expected = []byte(" my new reader for")
-	if !bytes.Equal(expected, buffer){
+	if !bytes.Equal(expected, buffer) {
 		t.Error("Read failed to extract correct data for part 2")
 	}
+}
+
+func TestPanicInChunking(t *testing.T) {
+	defer func() {
+		r := recover()
+		if r == nil {
+			t.Errorf("chunked reading should have paniced with EOF error")
+		} else {
+			t.Log("panic raised:", r)
+		}
+	}()
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	reader := strings.NewReader("Hello World")
+
+	part := chunk{size: 12, offset: int64(0)}
+	read(reader, part, wg)
 }
