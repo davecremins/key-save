29fa1ff079796b33031a79fac518c53a7e3de1b5
Dave Cremins <davecremins@gmail.com>
2018-09-07 21:12:21 +0100

    Split io operations into new package

--- io-ops/io_ops_test.go
+++ io-ops/io_ops_test.go
@@ -1,4 +1,4 @@
-package files
+package ioops
 
 import (
 	"bytes"
@@ -7,33 +7,33 @@ import (
 )
 
 func TestChunkCalculation(t *testing.T) {
-	chunks := prepareChunks(10089, 128)
-	expectedChunkCount := func(size, bufferSize int) int {
-		divideResult := size / bufferSize
-		remainder := size % bufferSize
+	chunks := PrepareChunks(10089, 128)
+	expectedChunkCount := func(Size, bufferSize int) int {
+		divideResult := Size / bufferSize
+		remainder := Size % bufferSize
 		if remainder > 0 {
 			divideResult++
 		}
 		return divideResult
 	}(10089, 128)
 	if expectedChunkCount != len(*chunks) {
-		t.Error("prepareChunks failed to create the correct size array")
+		t.Error("prepareChunks failed to create the correct Size array")
 	}
 }
 
 func TestChunkingOfReader(t *testing.T) {
 	reader := strings.NewReader("Testing my new reader for the files package")
 
-	part1 := chunk{size: 7, offset: int64(0)}
-	buffer := read(reader, part1)
+	part1 := Chunk{Size: 7, Offset: int64(0)}
+	buffer := Read(reader, part1)
 
 	expected := []byte("Testing")
 	if !bytes.Equal(expected, buffer) {
 		t.Error("read failed to extract correct data for part 1")
 	}
 
-	part2 := chunk{size: 18, offset: int64(7)}
-	buffer = read(reader, part2)
+	part2 := Chunk{Size: 18, Offset: int64(7)}
+	buffer = Read(reader, part2)
 
 	expected = []byte(" my new reader for")
 	if !bytes.Equal(expected, buffer) {
@@ -52,6 +52,6 @@ func TestPanicInChunking(t *testing.T) {
 	}()
 
 	reader := strings.NewReader("Hello World")
-	part := chunk{size: 12, offset: int64(0)}
-	read(reader, part)
+	part := Chunk{Size: 12, Offset: int64(0)}
+	Read(reader, part)
 }
