d480da2c67259dde833f4562c366b706cd79e91c
Dave Cremins <davecremins@gmail.com>
2018-09-12 22:12:46 +0100

    Initial file to hold file encryption code

---
+++ safe/protect.go
@@ -0,0 +1,10 @@
+package safe
+
+/*import (
+	cipher "github.com/davecremins/safe-deposit-box/cipher"
+	fr "github.com/davecremins/safe-deposit-box/files"
+)
+
+func ProtectFile(fileToProtect string, key []byte) err {
+
+}*/
