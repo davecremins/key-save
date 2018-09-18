3526e661ffd886a847f81c7d95cf102a603edca6
Dave Cremins <davecremins@gmail.com>
2018-09-18 13:35:17 +0100

    Refactor test not to use buffered channel

--- files/file_write_test.go
+++ files/file_write_test.go
@@ -7,11 +7,14 @@ import (
 
 func TestWritingToFileViaChannel(t *testing.T) {
 	var buf bytes.Buffer
-	byteChan := make(chan *[]byte, 2)
-	arg1, arg2 := []byte("Hello "), []byte("World")
-	byteChan <- &arg1
-	byteChan <- &arg2
-	close(byteChan)
+	byteChan := make(chan *[]byte)
+	go func(ch chan *[]byte) {
+		arg1, arg2 := []byte("Hello "), []byte("World")
+		ch <- &arg1
+		ch <- &arg2
+		close(ch)
+
+	}(byteChan)
 	write(&buf, byteChan, false)
 	if "Hello World" != buf.String() {
 		t.Error("write via channel returned unexpected result:", buf.String())
