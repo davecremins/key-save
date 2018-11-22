c7b47ffd6dd45f375ed224ae515f50b56e2815ff
Dave Cremins <davecremins@gmail.com>
2018-11-22 21:48:25 +0000

    Extract worker code into pipeline package

--- files/file_chunk.go
+++ files/file_chunk.go
@@ -4,23 +4,21 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"sync"
 
 	ops "gitlab.com/davecremins/safe-deposit-box/io-ops"
+	pipeline "gitlab.com/davecremins/safe-deposit-box/pipeline"
 )
 
-type job struct {
+type fileOp struct {
 	handle io.ReaderAt
 	data   *ops.Chunk
 }
 
-var showInfo = true
-
-func info(args ...interface{}) {
-	if showInfo {
-		fmt.Println(args...)
-	}
+func (op *fileOp) Execute() error {
+	ops.ReadIntoChunk(op.handle, op.data)
+	return nil
 }
+
 
 func ReadFileInChunks(filepath string, bufferSize int) *[]ops.Chunk {
 	file, err := os.Open(filepath)
@@ -41,44 +39,26 @@ func ReadFileInChunks(filepath string, bufferSize int) *[]ops.Chunk {
 func readInChunks(file io.ReaderAt, dataSize int64, bufferSize int) *[]ops.Chunk {
 	chunks := ops.PrepareChunks(dataSize, bufferSize)
 	chunkAmount := len(*chunks)
-	jobs := make(chan job, chunkAmount)
-	go allocateJobs(file, chunks, chunkAmount, jobs)
-
-	createWorkers(jobs, chunkAmount)
+	jobCh := pipeline.CreateJobPipe(chunkAmount)
+	pipeline.CreateWorkersForJobPipe(jobCh, 10)
+	pipeline.SendWorkToPipe(jobCh, allocateFileOps(file, chunks))
 	return chunks
 }
 
-func allocateJobs(file io.ReaderAt, chunks *[]ops.Chunk, chunkAmount int, jobs chan<- job) {
-	for i := 0; i < chunkAmount; i++ {
-		jobs <- job{handle: file, data: &(*chunks)[i]}
+func allocateFileOps(file io.ReaderAt, chunks *[]ops.Chunk) *[]pipeline.Job {
+	amount := len(*chunks)
+	fileOps := make([]pipeline.Job, amount)
+	for i := 0; i < amount; i++ {
+		fileOps[i] = &fileOp{handle: file, data: &(*chunks)[i]}
 	}
-	close(jobs)
+	return &fileOps
 }
 
 func processResults(jobResults <-chan *[]byte, done chan<- int) {
 	totalByteCount := 0
 	for bRead := range jobResults {
 		totalByteCount += len(*bRead)
-		info("Bytes read:", string(*bRead))
+		fmt.Println("Bytes read:", string(*bRead))
 	}
 	done <- totalByteCount
-}
-
-func createWorkers(jobs chan job, chunkAmount int) {
-	info(fmt.Sprintf("Creating %d workers to read file", chunkAmount))
-	var wg sync.WaitGroup
-	for w := 0; w < chunkAmount; w++ {
-		wg.Add(1)
-		go readWorker(w+1, jobs, &wg)
-	}
-	wg.Wait()
-}
-
-func readWorker(id int, jobs <-chan job, wg *sync.WaitGroup) {
-	for j := range jobs {
-		info("Processing job in worker:", id)
-		ops.ReadIntoChunk(j.handle, j.data)
-		info("Finished processing job in worker:", id)
-	}
-	wg.Done()
 }
