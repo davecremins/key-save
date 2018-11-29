9026206b364b2af98260c1f59131a9ff8b883bd3
Dave Cremins <davecremins@gmail.com>
2018-11-29 13:25:36 +0000

    Fixed failing test

--- key-mgt/load_key_test.go
+++ key-mgt/load_key_test.go
@@ -1,16 +1,20 @@
 package keymgt
 
-import "testing"
+import (
+	"testing"
+	"bytes"
+)
 
-// TODO: Fix failing test
-func TestCanDecodeBytesToPemBlock(t *testing.T){
+func TestCanDecodePemEncodedKeyToPemBlock(t *testing.T){
 	privateKey, _, _ := CreateRSAKeys(1024)
-	encodingStruct, _ := createPemEncodingStructureForKey(privateKey)
-	block, err := decodeBytesToPemBlock(&encodingStruct.block.Bytes)
-	t.Log(block)
+	out := new(bytes.Buffer)
+	PemEncodeKeyToOutput(privateKey, out)
+	pemEncodedBytes := out.Bytes()
+	block, err := decodeBytesToPemBlock(&pemEncodedBytes)
 	if err != nil {
 		t.Error("failed to decode pem block:", err)
 	}
+	t.Log(block)
 }
 
 func TestCanConvertPemBlockToPublicKey(t *testing.T){}
