e82ce2a293b105f9bb7b5887463649ab71b8a79b
Dave Cremins <davecremins@gmail.com>
2018-12-08 18:36:56 +0000

    Removed unused function and updated test

--- files/file_write.go
+++ files/file_write.go
@@ -1,28 +1,12 @@
 package files
 
-import (
-	"io"
-	"os"
-)
+import "io"
 
-const separator = "|"
-
-func CreateAndWaitToWrite(filename string, bytesToWrite chan *[]byte, includeSeparator bool) {
-	handle, err := os.Create(filename)
-	if err != nil {
-		panic(err)
-	}
-	defer handle.Close()
-	write(handle, bytesToWrite, includeSeparator)
-	handle.Sync()
-}
-
-// TODO: Write test
-func write(handle io.Writer, bytesToWrite chan *[]byte, includeSeparator bool) {
-	for content := range bytesToWrite {
-		handle.Write(*content)
-		if includeSeparator {
-			handle.Write([]byte(separator))
+func WaitToWrite(out io.Writer, bytesToWrite chan *[]byte, finished chan bool) {
+	go func(){
+		for content := range bytesToWrite {
+			out.Write(*content)
 		}
-	}
+		finished <- true
+	}()
 }
