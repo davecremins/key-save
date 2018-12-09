4568e40b1985ed6f74398f6e1572e8f199409c5b
Dave Cremins <davecremins@gmail.com>
2018-12-09 11:38:08 +0000

    Refactor by removing useless code

--- files/file_chunk.go
+++ files/file_chunk.go
@@ -1,9 +1,7 @@
 package files
 
 import (
-	"fmt"
 	"io"
-	"os"
 
 	ops "gitlab.com/davecremins/safe-deposit-box/io-ops"
 	pipeline "gitlab.com/davecremins/safe-deposit-box/pipeline"
@@ -19,49 +17,24 @@ func (op *fileOp) Execute() error {
 	return nil
 }
 
-func ReadFileInChunks(filepath string, bufferSize int) *[]ops.Chunk {
-	file, err := os.Open(filepath)
-	if err != nil {
-		fmt.Println(err)
-		return nil
-	}
-	defer file.Close()
-
-	fileinfo, err := file.Stat()
-	if err != nil {
-		fmt.Println(err)
-		return nil
-	}
-	return readInChunks(file, fileinfo.Size(), bufferSize)
-}
-
-func readInChunks(file io.ReaderAt, dataSize int64, bufferSize int) *[]ops.Chunk {
+func ReadInChunks(reader io.ReaderAt, dataSize int64, bufferSize int) *[]ops.Chunk {
 	chunks := ops.PrepareChunks(dataSize, bufferSize)
 	chunkAmount := len(*chunks)
 	pipelineConfig := pipeline.Config{
 		JobSize:      chunkAmount,
 		WorkerAmount: 10,
-		Jobs:         allocateFileOps(file, chunks),
+		Jobs:         allocateFileOps(reader, chunks),
 		LoadBalance:  false,
 	}
 	pipeline.Create(pipelineConfig)
 	return chunks
 }
 
-func allocateFileOps(file io.ReaderAt, chunks *[]ops.Chunk) *[]pipeline.Job {
+func allocateFileOps(reader io.ReaderAt, chunks *[]ops.Chunk) *[]pipeline.Job {
 	amount := len(*chunks)
 	fileOps := make([]pipeline.Job, amount)
 	for i := 0; i < amount; i++ {
-		fileOps[i] = &fileOp{handle: file, data: &(*chunks)[i]}
+		fileOps[i] = &fileOp{handle: reader, data: &(*chunks)[i]}
 	}
 	return &fileOps
-}
-
-func processResults(jobResults <-chan *[]byte, done chan<- int) {
-	totalByteCount := 0
-	for bRead := range jobResults {
-		totalByteCount += len(*bRead)
-		fmt.Println("Bytes read:", string(*bRead))
-	}
-	done <- totalByteCount
 }
