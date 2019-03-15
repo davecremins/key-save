f44c5f12012d82a7e74d70c2614783b18abb1894
Dave Cremins <davecremins@gmail.com>
2019-03-15 14:54:12 +0000

    Add encryption process

--- apps/cli.go
+++ apps/cli.go
@@ -1,8 +1,12 @@
 package apps
 
 import (
+	"os"
+	"io/ioutil"
 	"flag"
 	log "github.com/sirupsen/logrus"
+	km "gitlab.com/davecremins/safe-deposit-box/key-mgt"
+	"gitlab.com/davecremins/safe-deposit-box/cipher"
 )
 
 const (
@@ -20,7 +24,7 @@ func (c *CLI) Run() {
 	log.Info("Starting CLI")
 
 	operationPtr := flag.String("op", "encrypt", "Operation to perform - encrypt|decrypt")
-	keyPtr := flag.String("key", empty, "Security key")
+	//keyPtr := flag.String("key", empty, "Security key")
 	dataLocationPtr := flag.String("datapath", empty, "Path to file containing data")
 
 	flag.Parse()
@@ -34,11 +38,59 @@ func (c *CLI) Run() {
 		log.Fatal("Unsupported operation requested")
 	}
 
-	if *keyPtr == empty {
+	/*if *keyPtr == empty {
 		log.Fatal("Requested operation requires a key")
-	}
+	}*/
 
 	if *dataLocationPtr == empty {
 		log.Fatal("Requested operation requires a path to the file containing the data")
 	}
+
+	// Open file from data location
+	file, err := os.Open(*dataLocationPtr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	// Read in file
+	fileData, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Generate random key
+	key, err := km.CreateRandomKeyBytes(24)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// encrypt the file
+	encrypted, err := cipher.AESGCMEncrypt(&fileData, &key)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// write to new file
+	encryptedFile, err := os.OpenFile(
+		"encrypted.data",
+		os.O_WRONLY|os.O_TRUNC|os.O_CREATE,
+		0666,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer encryptedFile.Close()
+
+	// Write bytes to file
+	bytesWritten, err := encryptedFile.Write(encrypted)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Info("Wrote %d bytes.\n", bytesWritten)
+
+	// delete original
+
+	// show key to user
+	log.Info("Key used during encryption process:", string(key[:])) 
 }
