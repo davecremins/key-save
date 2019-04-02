768f4587e633877e8b083909f922d3af099f3267
Dave Cremins <davecremins@gmail.com>
2019-04-02 12:44:38 +0100

    Linting issues

--- files/file_write.go
+++ files/file_write.go
@@ -2,6 +2,8 @@ package files
 
 import "io"
 
+// WaitToWrite is a goroutine that will write bytes to an io.Writer
+// and signal that it is finished when the bytes channel is closed.
 func WaitToWrite(out io.Writer, bytesToWrite chan *[]byte, finished chan bool) {
 	go func() {
 		for content := range bytesToWrite {
